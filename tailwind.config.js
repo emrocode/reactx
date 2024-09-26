0eb70f86f537b36420389322b5bc74066b51d91d
emrocode <92260849+emrocode@users.noreply.github.com>
2024-09-26 11:47:53 -0400

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,8 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {
-      colors: {
-        reactx: {
-          100: "#16181d",
-          200: "#23272f",
-          300: "#343a46",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
+  darkMode: ["selector", "[data-theme='dark']"],
 };

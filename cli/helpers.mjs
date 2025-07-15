71d9593b32e30167e8bac1d23eb3d6808ee7c8b5
emrocode <92260849+emrocode@users.noreply.github.com>
2025-07-15 13:01:04 -0300

    Updates

--- cli/helpers.mjs
+++ cli/helpers.mjs
@@ -1,6 +1,12 @@
+import os from "os";
+
+const isWindows = os.platform() === "win32";
+
 // Check if command exists
 export async function hasCmd(cmd) {
   try {
+    isWindows ? await $`where ${cmd}` : await $`command -v ${cmd}`;
+
     await $`${cmd} --version`;
     return true;
   } catch {

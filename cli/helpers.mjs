aafd3e95cfb3a1b88de9a20564c8d6c0c22209b4
emrocode <92260849+emrocode@users.noreply.github.com>
2025-07-17 11:14:14 -0300

    Add cross-platform command checker using execa

--- cli/helpers.mjs
+++ cli/helpers.mjs
@@ -1,13 +1,16 @@
 import os from "os";
+import { execa } from "execa";
 
 const isWindows = os.platform() === "win32";
 
 // Check if command exists
 export async function hasCmd(cmd) {
   try {
-    isWindows ? await $`where ${cmd}` : await $`command -v ${cmd}`;
+    isWindows
+      ? await execa({ shell: true })`where ${cmd}`
+      : await execa({ shell: true })`command -v ${cmd}`;
 
-    await $`${cmd} --version`;
+    await execa({ shell: true })`${cmd} --version`;
     return true;
   } catch {
     return false;

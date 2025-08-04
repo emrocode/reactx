9a3857c469b4ffc83d67c2777a09ff03e40c566f
emrocode <92260849+emrocode@users.noreply.github.com>
2025-08-04 15:31:07 -0300

    Add grid layout (styles)

--- templates/js/src/components/Layout.jsx
+++ templates/js/src/components/Layout.jsx
@@ -4,7 +4,9 @@ import { Outlet } from "react-router";
 const Layout = () => {
   return (
     <>
+      <div className="grid h-16 w-full place-items-center border-b border-white/15 text-xs text-white italic after:content-['Header_section']" />
       <Outlet />
+      <div className="grid h-16 w-full place-items-center border-t border-white/15 text-xs text-white italic after:content-['Footer_section']" />
     </>
   );
 };

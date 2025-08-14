1fb158c058e26442f096d4abd5b52460674d226f
emrocode <92260849+emrocode@users.noreply.github.com>
2025-08-14 14:08:37 -0300

    Minor changes

--- templates/js/src/components/Layout.jsx
+++ templates/js/src/components/Layout.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import { Outlet } from "react-router";
 
 const Layout = () => {
   return (
     <>
-      <div className="grid h-16 w-full place-items-center border-b border-white/15 text-xs text-white italic after:content-['Header_section']" />
+      <div className="grid size-full min-h-16 place-items-center border-b border-white/15 text-xs text-white italic after:content-['Header_section']" />
       <Outlet />
-      <div className="grid h-16 w-full place-items-center border-t border-white/15 text-xs text-white italic after:content-['Footer_section']" />
+      <div className="grid size-full min-h-16 place-items-center border-t border-white/15 text-xs text-white italic after:content-['Footer_section']" />
     </>
   );
 };
